a799c88167472277991e496c9c97b8d11664c62d
jpbaruteu <73549131+jpbaruteu@users.noreply.github.com>
2020-10-28 16:18:30 +0100

    Update cloudbuild.tf

--- cloudbuild.tf
+++ cloudbuild.tf
@@ -47,55 +47,6 @@ resource "google_cloudbuild_trigger" "default" {
   }
 }
 
-resource "google_cloudbuild_trigger" "default" {
-  name = "dev"
-  trigger_template {
-    branch_name = "dev"
-    repo_name   = var.git_repo
-    dir         = "app"
-  }
-
-  build {
-    step {
-      name = "gcr.io/cloud-builders/docker"
-      args = ["build", "-t", "gcr.io/${var.project_id}/app:${COMMIT_REF_NAME}_${COMMIT_SHA}", "."]
-    }
-
-    step {
-      name = "gcr.io/cloud-builders/docker"
-      args = [
-        "run",
-        "gcr.io/${var.project_id}/app:${COMMIT_REF_NAME}_${COMMIT_SHA}",
-        "./vendor/bin/phpunit"
-      ]
-    }
-
-    step {
-      name = "gcr.io/cloud-builders/docker"
-      args = [
-        "push",
-        "gcr.io/${var.project_id}/app:${COMMIT_REF_NAME}_${COMMIT_SHA}"
-      ]
-    }
-
-    step {
-      name       = "gcr.io/google.com/cloudsdktool/cloud-sdk"
-      entrypoint = "gcloud"
-      args = [
-        "run",
-        "deploy",
-        "${var.project_id}-dev-srv",
-        "--image",
-        "gcr.io/${var.project_id}/app:${COMMIT_REF_NAME}_${COMMIT_SHA}",
-        "--region",
-        var.region,
-        "--platform",
-        "managed"
-      ]
-    }
-  }
-}
-
 resource "google_project_iam_binding" "cloudbuild_iam" {
   project = var.project_id
   role    = "roles/run.admin"
