ef7f566f01a912a595d27de540493b46e11d64a7
jpbaruteu <73549131+jpbaruteu@users.noreply.github.com>
2020-10-28 16:15:07 +0100

    Update cloudsql.tf

--- cloudsql.tf
+++ cloudsql.tf
@@ -15,6 +15,23 @@ resource "google_sql_database_instance" "default" {
   }
 }
 
+resource "google_sql_database_instance" "dev" {
+  name   = "${var.project_id}-db-${random_id.google_sql_database.hex}"
+  region = var.region
+  database_version = "MYSQL_5_7"
+
+  settings {
+    tier = "db-f1-micro"
+    availability_type = "ZONAL"
+
+    ip_configuration {
+      authorized_networks {
+        value = "0.0.0.0/0"
+      }
+    }
+  }
+}
+
 resource "random_id" "google_sql_database" {
   byte_length = 8
 }
@@ -33,4 +50,20 @@ resource "google_sql_user" "default" {
   password = var.sql_user.password
 
   depends_on = [google_sql_database_instance.default]
+}
+
+resource "google_sql_database" "dev" {
+  name     = var.sql_db_name
+  instance = google_sql_database_instance.dev.name
+
+  depends_on = [google_sql_database_instance.dev]
+}
+
+resource "google_sql_user" "dev" {
+  name     = var.sql_user.username
+  instance = google_sql_database_instance.dev.name
+  host     = "%"
+  password = var.sql_user.password
+
+  depends_on = [google_sql_database_instance.dev]
 }
